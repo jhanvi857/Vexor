4a3ce24167fac2b72bc4b27442da26e339782c70
jhanvi857 <jhanvip8507@gmail.com>
2026-05-24 22:46:56 +0530

    feat : add token bucket and fixed window rate limiting algorithms

---
+++ ratelimit/strategy/fixed_window.go
@@ -0,0 +1,18 @@
+type FixedWindow struct {
+	Limit       int
+	Count       int
+	WindowStart time.Time
+	mu          sync.Mutex
+}
+
+func (fw *FixedWindow) Allow() bool {
+	fw.mu.Lock()
+	defer fw.mu.Unlock()
+	now := time.Now()
+	if now.Sub(fw.WindowStart) >= time.Minute {
+		fw.Count = 0
+		fw.WindowStart = now
+	}
+	fw.Count++
+	return fw.Count <= fw.Limit
+}
