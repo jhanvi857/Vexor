36ca57d30bd319c0a60d6745c844670ffc6a437d
jhanvi857 <jhanvip8507@gmail.com>
2026-05-30 10:55:30 +0530

    add : middlewares in gateway

--- internal/gateway/handler.go
+++ internal/gateway/handler.go
@@ -1,26 +1,30 @@
 package gateway
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/jhanvi857/vexor/internal/proxy"
 	"github.com/jhanvi857/vexor/internal/routing"
 )
 
-func GatewayHandler(w http.ResponseWriter, r *http.Request) {
-	route := routing.MatchRoute(r.URL.Path)
-	if route == nil {
-		http.Error(w, "Route not found", http.StatusNotFound)
-		return
-	}
+func GatewayHandler() http.Handler {
 
-	proxy, err := proxy.NewProxy(route.Target)
-	if err != nil {
-		log.Printf("Error creating proxy for route %s: %v", route.Path, err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-	proxy.ServeHTTP(w, r)
+		route := routing.MatchRoute(r.URL.Path)
+
+		if route == nil {
+			http.NotFound(w, r)
+			return
+		}
+
+		proxyInstance, err := proxy.NewProxy(route.Target)
+
+		if err != nil {
+			http.Error(w, "Bad Gateway", http.StatusBadGateway)
+			return
+		}
+
+		proxyInstance.ServeHTTP(w, r)
+	})
 }
--- internal/gateway/server.go
+++ internal/gateway/server.go
@@ -3,10 +3,14 @@ package gateway
 import (
 	"log"
 	"net/http"
+
+	"github.com/jhanvi857/vexor/internal/middleware"
 )
 
 func Start() {
-	http.HandleFunc("/", GatewayHandler)
-	log.Println("Gateway started at port : 8080")
+	handler := GatewayHandler()
+	finalHandler := middleware.Chain(handler, middleware.Recovery, middleware.RequestID, middleware.Logger)
+	http.Handle("/", finalHandler)
+	log.Println("API gateway running on port : 8080")
 	http.ListenAndServe(":8080", nil)
 }
