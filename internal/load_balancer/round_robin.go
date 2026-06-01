0564ab852c5b1006725df1df19e0a9ec0e5c8895
jhanvi857 <jhanvip8507@gmail.com>
2026-06-01 10:31:59 +0530

    feat : add round robin algorithm for load balancing

---
+++ internal/load_balancer/round_robin.go
@@ -0,0 +1,36 @@
+package load_balancer
+
+import (
+	"errors"
+	"sync"
+)
+
+type RoundRobin struct {
+	instances []*Instance
+	current   int
+	mu        sync.Mutex
+}
+
+func NewRoundRobin(instances []*Instance) *RoundRobin {
+	return &RoundRobin{
+		instances: instances,
+		current:   0,
+	}
+}
+
+func (rr *RoundRobin) NextInstance() (*Instance, error) {
+	rr.mu.Lock()
+	defer rr.mu.Unlock()
+	n := len(rr.instances)
+	if n == 0 {
+		return nil, errors.New("no instances available")
+	}
+	for i := 0; i < n; i++ {
+		instance := rr.instances[rr.current]
+		rr.current = (rr.current + 1) % n
+		if instance.Healthy {
+			return instance, nil
+		}
+	}
+	return nil, errors.New("no healthy instances available")
+}
